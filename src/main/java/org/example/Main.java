bfc89649be26bde56f54a5093630f29465642159
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2023-02-26 00:46:52 +0100

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(
+                "Hello world!")
+
+
+        ;
+
+        System.out.println(
+                "my ugly ill formatted class ");}
+
+    public static void getString(){
+        System.out.println(
+                "really ill formatted ");}}
