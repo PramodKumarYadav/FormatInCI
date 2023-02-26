c51a47c711d7f1c8975ee68547e67cc4a2d63878
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2023-02-26 01:39:21 +0100

    another ill formatted file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,23 @@
 package org.example;
 
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+public
+class
+Main
+{
+  public static
+  void main(String[] args) {
+    System.out.println
+            ("Hello world!"
+            )
 
-    System.out.println("my ugly ill formatted class ");
+
+    ;
+
+    System.out.println
+            ("my ugly ill formatted class ");
   }
 
-  public static void getString() {
-    System.out.println("really ill formatted ");
-  }
-}
+  public
+  static void
+  getString         (){
+    System.out.println("really ill formatted ");}}
